da030098b71ed7b3a0df8b2869c87d3f90b0dd23
tayfuntemel <142969480+tayfuntemel@users.noreply.github.com>
2026-04-24 13:10:23 +0300

    Change scraper API key to use environment variable
    Updated scraper API key retrieval method to use environment variable.

--- maclari_guncelle122.py
+++ maclari_guncelle122.py
@@ -130,8 +130,8 @@ class DB:
 class Scraper:
     def __init__(self, cfg):
         self.cfg = cfg
-        # KENDİ SCRAPERAPI ANAHTARINI BURAYA YAPIŞTIRMALISIN
-        self.scraper_api_key = "48c0f9af1ba5cc24348d76ca5240faff"
+        # KENDİ SCRAPERAPI ANAHTARINI GITHUB SECRETS ÜZERİNDEN ALIYORUZ
+        self.scraper_api_key = os.getenv("SCRAPER_API_KEY")
 
     def start(self):
         print("[SİSTEM] ScraperAPI aktif! Maç Güncelleme botu başlatılıyor...")
