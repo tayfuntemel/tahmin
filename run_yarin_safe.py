8ec2d285a4703e05bd0310fadac38ef54f3422e6
tayfuntemel <142969480+tayfuntemel@users.noreply.github.com>
2026-05-05 15:56:58 +0300

    Delete run_yarin_safe.py

--- run_yarin_safe.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""Yarın için safe market tahmini üretir."""
-import datetime as dt
-from zoneinfo import ZoneInfo
-from safe_market_engine import run_single
-
-if __name__ == "__main__":
-    run_single(dt.datetime.now(ZoneInfo("Europe/Istanbul")).date() + dt.timedelta(days=1))
